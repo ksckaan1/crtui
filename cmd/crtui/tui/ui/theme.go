fd39baef9e6109076de2de2ae00be9f3eba9fad1
ksckaan1 <me@kaanksc.com>
2026-02-20 02:35:10 +0300

    add secondary color

--- cmd/crtui/tui/ui/theme.go
+++ cmd/crtui/tui/ui/theme.go
@@ -3,5 +3,6 @@ package ui
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	PrimaryColor = lipgloss.Color("#E60076")
+	PrimaryColor   = lipgloss.Color("#E60076")
+	SecondaryColor = lipgloss.Color("#383838")
 )
